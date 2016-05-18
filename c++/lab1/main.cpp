b32201cde8c70925c3702fe4ce03c34267f41475
Maxim Sheviakov <mrader3940@opmbx.org>
2016-05-18 17:34:14 +0400

    Implement some stuff

--- c++/lab1/main.cpp
+++ c++/lab1/main.cpp
@@ -2,8 +2,38 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+struct Order
 {
-    cout << "Hello World!" << endl;
+    double GetOrder, SendOrder, TransferAmount;
+};
+
+int sort (int *arr[], int size)
+{
+    if (size <= 0)
+        cerr << "Invalid size" << endl;
+
+    int *newarr = new int[size]; int temp;
+
+    /* Сортировка массива пузырьком */
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = newarr[j];
+                arr[j] = arr[j + 1];
+                newarr[j + 1] = temp;
+            }
+        }
+    }
+
+    return *newarr;
+}
+
+void GetCheckValue ()
+{
+
+}
+
+int main (int argc, char *argv[])
+{
     return 0;
 }
