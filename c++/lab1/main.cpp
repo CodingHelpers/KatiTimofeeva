b097b5b07689df5292cd704f413a0642504bec7e
Mike <lubinetsm@yandex.ru>
2016-05-19 19:38:53 +0300

    Finished lab1, added comments

--- c++/lab1/main.cpp
+++ c++/lab1/main.cpp
@@ -1,56 +1,87 @@
 #include <iostream>
+#include <cstring>
 
-using namespace std;
-
-struct Order
-{
-    public:
-        double PayAmount, GainAmount, TransferAmount;
+// Структура Order
+struct Order {
+public:
+    int InputAccount,   // расчетный счет плательщика
+        OutputAccount,  // расчетный счет получателя
+        Sum;            // перечисляемая сумма в рублях
 };
 
-double sum(const Order orders[], int index)
-{
-    return (orders[index].PayAmount + orders[index].GainAmount + orders[index].TransferAmount);
-}
-
-void swapValues (Order &order1, Order &order2)
-{
+// Обмен значениями между двумя заказами
+void swap(Order &order1, Order &order2) {
     Order temp;
-    temp = order2;
-    order2 = order1;
-    order1 = temp;
-    delete &temp;
+    temp = order1;
+    order1 = order2;
+    order2 = temp;
 }
 
-double sort (Order orders[], int size) /* size обычно есть sizeof(order)/sizeof(order[0]) */
-{
-    if (size <= 0)
-        cerr << "Неверный размер" << endl;
-
-    int temp;
-
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = 0; j < size - 1; j++)
-        {
-            if (sum(orders[], j) < sum(orders[], j + 1)) //FIXME
-                swapValues(orders[j], orders[j + 1]);
+// Сортировка массива заказов по убыванию суммы
+void sort(Order orders[], uint size) {
+    for (uint i = 0; i < size; i++) {
+        for (uint j = 0; j < size - 1; j++) {
+            if (orders[j].Sum < orders[j+1].Sum) {
+                swap(orders[j], orders[j+1]);
+            }
         }
     }
 }
 
-void GetCheckValue (Order orders[])
-{
-    int Payment;
-    cin >> Payment;
-    for (int i = 0; i < (sizeof(orders)/sizeof(orders[0])); i++)
-    {
-        if (Payment == orders->GainAmount)
-            cout << "Совпадение: " << orders->GainAmount << " в счёте №" << i << endl;
+// Вывод заказа на экран
+void print(const Order& order, uint n) {
+    std::cout << "Order #" << n << ": "
+              << order.InputAccount << "->" << order.OutputAccount << " "
+              << order.Sum << std::endl;
+}
+
+// Поиск заказов по счету плательщика и вывод на экран
+void GetCheck (Order orders[], uint size) {
+    // Ввод номера счета плательщика
+    int AccountNum;
+    std::cout << "Input Account Number: ";
+    std::cin >> AccountNum;
+
+    // Поиск в массиве заказов с введенным номером счета плательщика
+    bool found = false;
+    for(uint i = 0; i < size; i++) {
+        if(AccountNum == orders[i].InputAccount) {
+            found = true;
+            print(orders[i], i);
+        }
+    }
+
+    if(!found) {
+        std::cout << "No orders found\n";
     }
 }
 
-int main (int argc, char *argv[])
-{
+int main () {
+    // Создание нового массива структур
+    Order orders[5] = {
+        {1, 2, 30}, // 1 -- номер плательика, 2 - номер получателя, 30 -- сумма
+        {2, 3, 20},
+        {1, 5, 50},
+        {1, 10, 5},
+        {2, 4, 100}
+    };
+
+    // Вывод массива структур
+    std::cout << "Orders:\n";
+    for(uint i = 0; i < 5; i++) {
+        print(orders[i], i);
+    }
+
+    // Сортировка массива структур и вывод на экран
+    std::cout << "\nSorted Orders:\n";
+    sort(orders, 5);
+    for(uint i = 0; i < 5; i++) {
+        print(orders[i], i);
+    }
+
+    // Поиск в массиве структур
+    std::cout << std::endl;
+    GetCheck(orders, 5);
+
     return 0;
 }
